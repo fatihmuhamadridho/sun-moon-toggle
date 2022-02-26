80b7a476f69d0d51877b898ca3d631d02272fc5b
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-02-26 09:13:57 +0700

    first commit

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleBg() {
-    document.getElementById('containerBg').classList.toggle('containerBgstyle')
-    document.getElementById('toggle').classList.toggle("togglestyle")
-    document.getElementById('crescent').classList.toggle("crescentstyle")
-    document.getElementById('crescentBg').classList.toggle("crescentBgstyle")
-    document.getElementById('cloud2').classList.toggle("cloud2style")
-    document.getElementById('cloud1').classList.toggle("cloud1style")
-    document.getElementById('stars').classList.toggle("starsstyle")
-}
